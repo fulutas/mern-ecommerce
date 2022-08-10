6a31fba7a50926753c1b4079b13b1da5bef7a2c7
furkan <ulutasfurkan@gmail.com>
2022-08-10 15:30:22 +0300

    user update api added and verifyToken created

---
+++ routes/middleware/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token
+
+    if(authHeader){
+        const token  = authHeader.split(" ")[1]
+        jwt.verify(token, process.env.JWT_SEC, (err, userData) => {
+            // If the token is incorrect or expired, an error is returned.
+            if(err) res.status(403).json({ message : 'Token is not valid.'})
+            req.user = userData
+            next()
+        })
+    }else{
+        return res.status(401).json({ message : 'You are not authenticated.'})
+    }
+} 
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        }else{
+            res.status(403).json("You are not allowed to do that.")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorization }
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,27 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const { verifyToken, verifyTokenAndAuthorization } = require("./middleware/verifyToken");
 
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    
+   if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString()
+   }
+
+   try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        $set : req.body
+    }, { new : true})
+
+    res.status(200).json(updatedUser)
+
+   } catch (error) {
+    res.status(500).json(err)
+   }
+})
 
 module.exports = router
