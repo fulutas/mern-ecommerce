3f13f59cb25c91e27260304105fd8bd60d56b804
furkan <ulutasfurkan@gmail.com>
2022-08-11 12:09:17 +0300

    get user stats route add

--- routes/user.js
+++ routes/user.js
@@ -66,4 +66,29 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
     }
 })
 
+// GET USER STATS
+router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date()
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
+
+    try {
+        
+        const data = await User.aggregate([
+            { $match : { createdAt : { $gte : lastYear } }},
+            { $project : { 
+                month : { $month : "$createdAt" }
+            }},
+            { $group : {
+                _id : "$month",
+                total : { $sum : 1 },
+            }}
+        ])
+
+        res.status(200).json(data)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
