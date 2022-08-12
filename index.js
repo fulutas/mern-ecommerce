6f19e55a9660fb09470826746a66f35f1cc815db
furkan <ulutasfurkan@gmail.com>
2022-08-12 17:44:40 +0300

    stripe implements and cors

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors')
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 
 mongoose
@@ -23,12 +25,16 @@ mongoose
     console.log(err);
   });
 
+app.use(cors())  
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Backend server is running PORT : ${PORT}`);
