eb998c082db8581c0cb982b33cddfdbbe09aba9d
furkan <ulutasfurkan@gmail.com>
2022-08-20 15:49:15 +0300

    axios.js bringing Bearer token from local storage is provided

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZjM5MGU2NWYyMmE0ZWI0YmZhZmEyMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MDgxMzc0OSwiZXhwIjoxNjYxMDcyOTQ5fQ.MIgWejX15v__kRDz7f2LSYK4pu8vA6hCITgKOwc-mO8";
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accesToken;
 
 const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
